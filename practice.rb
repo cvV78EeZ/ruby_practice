eb337d937c3221dd708b65fba47fc4a66e04decd
Fumitaka <hyottoko8ojisann@gmail.com>
2023-07-31 03:33:39 +0000

    firstcommit

---
+++ practice.rb
@@ -0,0 +1,37 @@
+puts 'hello ruby!'
+a=3
+b=7
+c=10
+c=3+7
+puts c
+
+
+months=["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+puts months[7]
+
+hello="Hello"
+name="Tangor"
+world="'s World!"
+message=hello+name+world
+puts message
+
+omusubi_tech="omusubi"
+omusubi_tech+="tech"
+puts omusubi_tech
+
+_2023_calendar = {
+    "January" => "1月",
+    "February"=> "2月",
+    "March" => "3月",
+    "April" => "4月",
+    "May" => "5月",
+    "June" => "6月",
+    "July" => "7月",
+    "August" => "8月",
+    "September" => "9月",
+    "October" => "10月",
+    "November" => "11月",
+    "December" => "12月",
+}
+
+puts  _2023_calendar["December"]
